fbaf0266d631c5eebb861035427fd493ee5f5e11
sirasaki-konoha <rock-db@outlook.jp>
2025-08-28 13:39:55 +0900

    enhance launch_server.sh

--- launch_server.sh
+++ launch_server.sh
@@ -6,13 +6,54 @@ if ! command -v python 2>&1 >/dev/null; then
 	exit 1
 fi
 
-python -m http.server 8080 &
+
+ROOT=
+
+if [[ "$EUID" -ne 0 ]]; then
+	echo "request sudo..."
+	sudo -v
+	ROOT=0
+else
+	ROOT=1
+fi
+
+
+if [[ "$ROOT" -eq 1 ]]; then
+	python -m http.server 8080 >log.txt 2>&1  &
+else
+	sudo python -m http.server 8080 >log.txt 2>&1 &
+fi
+
+PID=$!
+
 if [[ "$?" -eq 0 ]]; then
-	echo "Server started http://127.0.0.1"
+	echo "Server started http://0.0.0.0:8080"
 else 
 	echo "Server start failed"
 	exit 1
 fi
 
-wait
+echo "Enter 'exit' to exit process."
+
+while true; do
+	read -p "> " val
+
+	case ${val} in
+		"exit")
+			printf "Killing python process..."
+			kill "$PID"
+			echo "done"
+			break
+			;;
+		"" | "\n" | " ")
+			continue;
+			;;
+
+		*)
+			echo "Unknown Command: $val"
+			continue
+			;;
+	esac
+done
+
 
